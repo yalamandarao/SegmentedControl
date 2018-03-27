dbd983ec40d19a7546bc6f2c79b543be7cd9234f
Yalamandarao <yalamandarao.i@gmail.com>
2018-03-27 12:29:14 +0800

    - iOS working fine

---
+++ SegmentedControl/Droid/Renderers/ButtonRenderer.cs
@@ -0,0 +1,231 @@
+﻿using System.ComponentModel;
+using Android.Content;
+using Android.Content.Res;
+using Android.Graphics.Drawables;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(SegmentedControl.Core.Controls.Button),
+                          typeof(SegmentedControl.Droid.Renderers.ButtonRenderer))]
+namespace SegmentedControl.Droid.Renderers
+{
+    public class ButtonRenderer : Xamarin.Forms.Platform.Android.ButtonRenderer
+    {
+        #region Properties
+
+        Core.Controls.Button BaseElement
+        {
+            get { return Element as Core.Controls.Button; }
+        }
+
+        #endregion
+
+        #region Initializations
+
+        public ButtonRenderer(Context context) : base(context)
+        {
+        }
+
+        #endregion
+
+        #region Life-cycle methods
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            if (Control == null
+                || Element == null)
+            {
+                return;
+            }
+
+            if (e.PropertyName == Core.Controls.Button.NormalBackgroundDrawableProperty.PropertyName
+                || e.PropertyName == Core.Controls.Button.PressedBackgroundDrawableProperty.PropertyName
+                || e.PropertyName == Core.Controls.Button.DisabledBackgroundDrawableProperty.PropertyName)
+            {
+                UpdateBackgroundDrawable();
+            }
+            else if (e.PropertyName == Core.Controls.Button.BackgroundColorProperty.PropertyName
+                     || e.PropertyName == Core.Controls.Button.PressedBackgroundColorProperty.PropertyName
+                     || e.PropertyName == Core.Controls.Button.DisabledTextColorProperty.PropertyName)
+            {
+                UpdateBackgroundColor();
+            }
+            else if (e.PropertyName == Core.Controls.Button.PressedTextColorProperty.PropertyName
+                     || e.PropertyName == Core.Controls.Button.DisabledTextColorProperty.PropertyName
+                     || e.PropertyName == Core.Controls.Button.TextColorProperty.PropertyName)
+            {
+                UpdateTextColor();
+            }
+
+            else if (e.PropertyName == Core.Controls.Button.CustomCornerRadiusProperty.PropertyName)
+            {
+                SetCornerRadius();
+            }
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Button> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control == null
+                || Element == null)
+            {
+                return;
+            }
+
+
+            Control.SetPadding(0, 0, 0, 0);
+            UpdateBackgroundDrawable();
+            UpdateBackgroundColor();
+            UpdateTextColor();
+            SetCornerRadius();
+
+        }
+
+        #endregion
+
+        #region Internal methods
+
+        void UpdateBackgroundDrawable()
+        {
+            string drawableName = BaseElement.NormalBackgroundDrawable?.File;
+            string pressedDrawable = BaseElement.PressedBackgroundDrawable?.File;
+            string disabledDrawable = BaseElement.DisabledBackgroundDrawable?.File;
+            if (drawableName != null
+                || pressedDrawable != null
+                || disabledDrawable != null)
+            {
+                var stateListDrawable = new StateListDrawable();
+
+                if (disabledDrawable != null)
+                {
+                    var drawable = Context.GetDrawable(disabledDrawable);
+                    stateListDrawable.AddState(
+                      new[] { -Android.Resource.Attribute.StateEnabled },
+                      drawable);
+                }
+
+                if (pressedDrawable != null)
+                {
+                    var drawable = Context.GetDrawable(pressedDrawable);
+                    stateListDrawable.AddState(
+                      new[] { Android.Resource.Attribute.StateEnabled,
+                    Android.Resource.Attribute.StatePressed},
+                      drawable);
+                }
+
+                if (drawableName != null)
+                {
+                    var drawable = Context.GetDrawable(drawableName);
+                    stateListDrawable.AddState(
+                      new[] { Android.Resource.Attribute.StateEnabled },
+                      drawable);
+                }
+                Control.Background = stateListDrawable;
+            }
+        }
+
+        new void UpdateBackgroundColor()
+        {
+            base.UpdateBackgroundColor();
+
+            Android.Graphics.Color normalColor = BaseElement.BackgroundColor.ToAndroid();
+            Android.Graphics.Color pressedColor = BaseElement.PressedBackgroundColor.ToAndroid();
+            Android.Graphics.Color disabledColor = BaseElement.DisabledBackgroundColor.ToAndroid();
+
+            if (pressedColor != 0
+                || disabledColor != 0)
+            {
+                var stateListColor = new StateListDrawable();
+
+                if (disabledColor != 0)
+                {
+                    stateListColor.AddState(
+                      new[] {
+              -Android.Resource.Attribute.StateEnabled
+                      },
+                      new ColorDrawable(disabledColor));
+                }
+
+                if (pressedColor != 0)
+                {
+                    stateListColor.AddState(
+                      new[] {
+              Android.Resource.Attribute.StateEnabled,
+              Android.Resource.Attribute.StatePressed
+                      },
+                      new ColorDrawable(pressedColor));
+                }
+
+                stateListColor.AddState(new[] { Android.Resource.Attribute.StateEnabled },
+                                        new ColorDrawable(normalColor));
+
+                Control.SetBackground(stateListColor);
+            }
+        }
+
+        void UpdateTextColor()
+        {
+            int normalColor = BaseElement.TextColor.ToAndroid();
+            int selectedColor = BaseElement.SelectedTextColor.ToAndroid();
+            int disabledColor = BaseElement.DisabledTextColor.ToAndroid();
+
+            var stateListColor = new ColorStateList(new int[][] {
+        new int[] { -Android.Resource.Attribute.StateEnabled
+        },
+        new int[] { Android.Resource.Attribute.StateEnabled,
+                    Android.Resource.Attribute.StatePressed
+        },
+        new int[] { Android.Resource.Attribute.StateEnabled,
+                    Android.Resource.Attribute.StateSelected
+        },
+        new int[] { Android.Resource.Attribute.StateEnabled
+        }
+      },
+            new int[] {
+        disabledColor == 0 ? normalColor : disabledColor,
+        selectedColor == 0 ? normalColor : selectedColor,
+        selectedColor == 0 ? normalColor : selectedColor,
+        normalColor,
+            });
+
+            Control.SetTextColor(stateListColor);
+        }
+
+
+
+        void SetCornerRadius()
+        {
+            var radius = (float)BaseElement.CustomCornerRadius;
+            if (!(radius > 0))
+            {
+                return;
+            }
+            base.UpdateBackgroundColor();
+            Android.Graphics.Color normalColor = BaseElement.BackgroundColor.ToAndroid();
+            Android.Graphics.Color pressedColor = BaseElement.PressedBackgroundColor.ToAndroid();
+
+            int[][] states = new int[2][]
+                      {
+        new int[] { Android.Resource.Attribute.StatePressed, 0},
+        new int[] { Android.Resource.Attribute.StateEnabled, 0}
+                      };
+
+            int[] colors = new int[2]
+                      {
+        pressedColor,
+        normalColor
+            };
+
+            GradientDrawable gradientDrawable = new GradientDrawable();
+            gradientDrawable.SetShape(ShapeType.Rectangle);
+            gradientDrawable.SetColor(new Android.Content.Res.ColorStateList(states, colors));
+            gradientDrawable.SetCornerRadius(BaseElement.CustomCornerRadius * 2);
+            Control.SetBackground(gradientDrawable);
+        }
+
+        #endregion
+    }
+}
---
+++ SegmentedControl/iOS/Renderers/ButtonRenderer.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+using CoreGraphics;
+using SegmentedControl.Core.Controls;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(SegmentedControl.Core.Controls.Button),
+                          typeof(SegmentedControl.iOS.Renderers.ButtonRenderer))]
+namespace SegmentedControl.iOS.Renderers
+{
+    public class ButtonRenderer : Xamarin.Forms.Platform.iOS.ButtonRenderer
+    {
+        #region Properties
+
+        UIButton _button;
+
+        Core.Controls.Button BaseElement
+        {
+            get
+            {
+                return Element as Core.Controls.Button;
+            }
+        }
+
+        #endregion
+
+        #region Life-cycle methods
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing
+                && Control != null)
+            {
+                Control.TouchUpInside -= TouchUpInside;
+            }
+            base.Dispose(disposing);
+        }
+
+        protected override void OnElementPropertyChanged(object sender,
+                                                         PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            if (Element == null
+                || BaseElement == null)
+            {
+                return;
+            }
+
+
+            if (e.PropertyName == Core.Controls.Button.BackgroundColorProperty.PropertyName)
+            {
+                UpdateNormalBackgroundColor();
+            }
+            else if (e.PropertyName == Core.Controls.Button.CustomCornerRadiusProperty.PropertyName)
+            {
+                UpdatePressedBackgroundColor();
+            }
+            else if (e.PropertyName == Core.Controls.Button.PressedBackgroundColorProperty.PropertyName)
+            {
+                UpdateCornerRadius();
+            }
+            else if (e.PropertyName == Core.Controls.Button.DisabledTextColorProperty.PropertyName)
+            {
+                UpdateDisabledBackgroundColor();
+            }
+            else if (e.PropertyName == Core.Controls.Button.NormalBackgroundDrawableProperty.PropertyName)
+            {
+                UpdateNormalBackgroundDrawable();
+            }
+            else if (e.PropertyName == Core.Controls.Button.PressedBackgroundDrawableProperty.PropertyName)
+            {
+                UpdatePressedBackgroundDrawable();
+            }
+            else if (e.PropertyName == Core.Controls.Button.DisabledBackgroundDrawableProperty.PropertyName)
+            {
+                UpdateDisabledBackgroundDrawable();
+            }
+            else if (e.PropertyName == Core.Controls.Button.PressedTextColorProperty.PropertyName)
+            {
+                UpdateSelectedTextColor();
+            }
+            else if (e.PropertyName == Core.Controls.Button.DisabledTextColorProperty.PropertyName)
+            {
+                UpdateDisabledTextColor();
+            }
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Button> e)
+        {
+            base.OnElementChanged(e);
+            InitButton();
+
+            if (Control == null
+                || BaseElement == null)
+            {
+                return;
+            }
+            Control.Layer.CornerRadius = (nfloat)5.0;
+            UpdateDisabledBackgroundDrawable();
+            UpdatePressedBackgroundDrawable();
+            UpdateNormalBackgroundDrawable();
+            UpdateNormalBackgroundColor();
+            UpdatePressedBackgroundColor();
+            UpdateDisabledBackgroundColor();
+            UpdateCornerRadius();
+            UpdateDisabledTextColor();
+            UpdateDisabledTextColor();
+            UpdateSelectedTextColor();
+        }
+
+        #endregion
+
+        #region Internal methods
+
+        void InitButton()
+        {
+            if (_button == null
+                && Control != null)
+            {
+                _button = new UIButton(UIButtonType.Custom);
+                SetNativeControl(_button);
+                Control.TouchUpInside += TouchUpInside;
+
+                InvokeMethod(this, "UpdateText", null);
+                InvokeMethod(this, "UpdateFont", null);
+                InvokeMethod(this, "UpdateBorder", null);
+                InvokeMethod(this, "UpdateImage", null);
+                InvokeMethod(this, "UpdateTextColor", null);
+            }
+        }
+
+        void TouchUpInside(object sender, EventArgs e)
+        {
+            InvokeMethod(this, "OnButtonTouchUpInside", sender, e);
+        }
+
+        void UpdateNormalBackgroundColor()
+        {
+            if (BaseElement.BackgroundColor == Color.Default)
+            {
+                return;
+            }
+
+            UIImage source = ImageFromColor(BaseElement.BackgroundColor.ToUIColor());
+            Control.SetBackgroundImage(source, UIControlState.Normal);
+        }
+
+        void UpdatePressedBackgroundColor()
+        {
+            if (BaseElement.PressedBackgroundColor == Color.Default)
+            {
+                return;
+            }
+
+            UIImage source = ImageFromColor(BaseElement.PressedBackgroundColor.ToUIColor());
+            Control.SetBackgroundImage(source, UIControlState.Highlighted);
+            Control.SetBackgroundImage(source, UIControlState.Selected);
+        }
+
+        void UpdateCornerRadius()
+        {
+            Control.Layer.CornerRadius = (nfloat)BaseElement.CustomCornerRadius;
+            Control.ClipsToBounds = true;
+        }
+
+        void UpdateDisabledBackgroundColor()
+        {
+            if (BaseElement.DisabledBackgroundColor == Color.Default)
+            {
+                return;
+            }
+
+            UIImage source = ImageFromColor(BaseElement.DisabledBackgroundColor.ToUIColor());
+            Control.SetBackgroundImage(source, UIControlState.Disabled);
+        }
+
+        UIImage ImageFromColor(UIColor color)
+        {
+            CGRect rect = new CGRect(0.0f, 0.0f, 1.0f, 1.0f);
+            UIGraphics.BeginImageContext(rect.Size);
+            CGContext context = UIGraphics.GetCurrentContext();
+
+            context.SetFillColor(color.CGColor);
+            context.FillRect(rect);
+
+            UIImage image = UIGraphics.GetImageFromCurrentImageContext();
+            UIGraphics.EndImageContext();
+
+            return image;
+        }
+
+        void UpdateNormalBackgroundDrawable()
+        {
+            string drawableName = BaseElement.NormalBackgroundDrawable?.File;
+            if (drawableName == null)
+            {
+                return;
+            }
+
+            UIImage source = CreateBackgroundImage(drawableName);
+            Control.SetBackgroundImage(source, UIControlState.Normal);
+        }
+
+        void UpdatePressedBackgroundDrawable()
+        {
+            string drawableName = BaseElement.PressedBackgroundDrawable?.File;
+            if (drawableName == null)
+            {
+                return;
+            }
+
+            UIImage source = CreateBackgroundImage(drawableName);
+
+            Control.AdjustsImageWhenHighlighted = source != null;
+
+            Control.SetBackgroundImage(source, UIControlState.Highlighted);
+            Control.SetBackgroundImage(source, UIControlState.Selected);
+        }
+
+        void UpdateDisabledBackgroundDrawable()
+        {
+            string drawableName = BaseElement.DisabledBackgroundDrawable?.File;
+            if (drawableName == null)
+            {
+                return;
+            }
+
+            UIImage source = CreateBackgroundImage(drawableName);
+            Control.SetBackgroundImage(source, UIControlState.Disabled);
+        }
+
+        UIImage CreateBackgroundImage(string drawableName)
+        {
+            UIImage source = UIImage.FromBundle(drawableName);
+            if (!Equals(BaseElement.EdgeInsets, default(EdgeInsets)))
+            {
+                var mode = BaseElement.ResizeMode == ResizeMode.Stretch
+                          ? UIImageResizingMode.Stretch
+                          : UIImageResizingMode.Tile;
+
+                source = source.CreateResizableImage(
+                  new UIEdgeInsets(BaseElement.EdgeInsets.Top,
+                                   BaseElement.EdgeInsets.Left,
+                                   BaseElement.EdgeInsets.Bottom,
+                                   BaseElement.EdgeInsets.Right),
+                  mode
+                );
+                Control.ContentMode = UIViewContentMode.ScaleToFill;
+            }
+            return source;
+        }
+
+        void UpdateDisabledTextColor()
+        {
+            UIColor color = BaseElement.DisabledTextColor.ToUIColor();
+            Control.SetTitleColor(color, UIControlState.Disabled);
+        }
+
+        void UpdateSelectedTextColor()
+        {
+            UIColor color = (BaseElement.SelectedTextColor == Color.Default
+                            ? BaseElement.TextColor
+                            : BaseElement.SelectedTextColor).ToUIColor();
+
+            Control.SetTitleColor(color, UIControlState.Highlighted);
+            Control.SetTitleColor(color, UIControlState.Selected);
+        }
+
+        #endregion
+
+        public object InvokeMethod(object target, string methodName, params object[] args)
+        {
+            Type t = target.GetType();
+            MethodInfo mi = null;
+
+            while (t != null)
+            {
+                mi = t.GetMethod(methodName, BindingFlags.NonPublic | BindingFlags.Instance);
+
+                if (mi != null) break;
+
+                t = t.BaseType;
+            }
+
+            if (mi == null)
+            {
+                throw new Exception(string.Format("Method '{0}' not found in type hierarchy.", methodName));
+            }
+            return mi.Invoke(target, args);
+        }
+    }
+}
